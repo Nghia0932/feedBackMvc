48b8e5d295ac331f471a4fb5bffcc490a38febf9
Nghia0932 <Nghiatran00932@gmail.com>
2024-09-19 00:57:34 +0700

    update Show_OUT_MauKHaoSat thêm mức đánh giá

--- Controllers/OutPatients/OUT_MauKhaoSat.cs
+++ Controllers/OutPatients/OUT_MauKhaoSat.cs
@@ -50,6 +50,7 @@ namespace feedBackMvc.Controllers.OutPatients
             public List<string>? NhomCauHoi { get; set; }
             public List<string>? TieuDeCauHoi { get; set; }
             public List<double>? MucQuanTrong { get; set; }
+            public List<int>? MucDanhGia { get; set; }
         }
         [HttpPost]
         public async Task<IActionResult> ThemMauKhaoSat([FromBody] ThemMauKhaoSat_Request request)
@@ -83,7 +84,8 @@ namespace feedBackMvc.Controllers.OutPatients
                 TrangThai = false,
                 HienThi = true,
                 Xoa = false,
-                MucQuanTrong = request.MucQuanTrong?.ToArray()
+                MucQuanTrong = request.MucQuanTrong?.ToArray(),
+                MucDanhGia = request.MucDanhGia?.ToArray()
             };
             try
             {
