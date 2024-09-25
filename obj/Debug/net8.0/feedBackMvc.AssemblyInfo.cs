41892b19291e484beece867544ac61ac34bc35cc
Nghia0932 <Nghiatran00932@gmail.com>
2024-09-25 15:52:24 +0700

    add responsive Index.cshtml

--- obj/Debug/net8.0/feedBackMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/feedBackMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("feedBackMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ed00d70a5739b0223889faf5eb37f0054f94b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542178ef82aae228b5c0422ecdf123e589857e5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("feedBackMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("feedBackMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
